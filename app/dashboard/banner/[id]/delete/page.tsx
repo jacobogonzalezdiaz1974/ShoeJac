ab3825e5ace98a3abfc05dacba0d6ce085f32a7b
jacobogonzalezdiaz1974 <jacobogonzalez1974@outlook.com>
2024-08-28 22:37:38 +0100

    finished code

---
+++ app/dashboard/banner/[id]/delete/page.tsx
@@ -0,0 +1,33 @@
+
+import { deleteBanner } from "@/app/actions";
+import { SubmitButton } from "@/app/components/SubmitButtons";
+import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+
+export default function DeleteBannerRoute({params}:{params:{id:string}  }) {
+  return (
+    <div className="h-[80vh] w-full items-center justify-center">
+      <Card className="max-w-xl">
+        <CardHeader>
+          <CardTitle>
+            Are you absolutely sure you want to delete this product?
+          </CardTitle>
+          <CardDescription>
+            This action cannot be undone. This will permanently delete this
+            banner and remove all data from our servers.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="w-full flex justify-between">
+          <Button variant="secondary" asChild>
+            <Link href="/dashboard/banner">Cancel</Link>
+          </Button>
+          <form action={deleteBanner}>
+            <input type="hidden" name="bannerId" value={params.id} />
+            <SubmitButton variant="destructive" text="Delete Product" />
+          </form>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
